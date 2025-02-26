15d94442a7607760bbe86596b6ca2cc4c5381585
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2025-02-27 02:39:13 +0530

    auth controller changed

--- controllers/authController.js
+++ controllers/authController.js
@@ -178,3 +178,17 @@ module.exports.googleAuthCallback=async (req, res) => {
     // Send a response indicating successful logout
     res.status(200).json({ message: 'Logged out successfully' });
   }
+
+
+module.exports.fetchUser=async (req, res) => {
+      try {
+          const user = await User.findById(req.user.id).select("-password"); // Exclude password
+          if (!user) {
+              return res.status(404).json({ message: "User not found" });
+          }
+          res.json(user);
+      } catch (error) {
+          console.error("Error fetching user:", error);
+          res.status(500).json({ message: "Server error" });
+      }
+    }
